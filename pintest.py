1ceb62d16686a5e2dc1a1b1f8490b9d95c8c07c8
Norbert.Schwarz <info@datentechnikschwarz.de>
2022-05-17 19:53:41 +0200

    Init

--- pintest.py
+++ pintest.py
@@ -16,43 +16,7 @@ def main():
     
 
 
-    # while MySerial.sercon_read_flag():
-
-    #     MySerial.sercon_read_line()
-    #     if MySerial.get_ready_flag():       # Zeichenkette empfangen
-    #         #print(MySerial.get_string())
-    #         MyDecode.decode_input(str(MySerial.get_string()))
-    #         #MyDecode.decode_printout()
-    #         if MyDecode.get_valid_flag() == True:
-    #             #print("Valid Command")
-    #             if MyDecode.get_cmd_1() == "do":
-    #                 #print("do")
-    #                 if MyDecode.get_cmd_2() == "all":
-    #                     #print("all")
-    #                     if MyDecode.get_value_1() == 0:
-    #                         #print("off")
-    #                         MyWS2812.do_all_off()
-    #                     if MyDecode.get_value_1() == 1:
-    #                         #print("on")
-    #                         MyWS2812.do_all_on()
-    #                     if MyDecode.get_value_1() == 2:
-    #                         #print("def")
-    #                         MyWS2812.do_all_def()
-    #                 if MyDecode.get_cmd_2() == "obj":
-    #                     #print("obj")
-    #                     #print(MyDecode.get_value_1())
-    #                     #print(segment_map[MyDecode.get_value_1()])
-    #                     #MyWS2812.set_led_obj(segment_map[MyDecode.get_value_1()], MyDecode.get_value_2())
-    #                     pass
-
-    #     # Loop-Delay !!!
-    #     time.sleep(0.01)        # 10ms
-        
-        
-
-        
-
-
+ 
 
     print("=== End of Main ===")
 
@@ -65,33 +29,6 @@ def main():
 
 
 if __name__ == "__main__":
-
-    if MyModule.inc_ws2812:
-        #print("WS2812 -> Load-Module")
-        import module_ws2812_v3 as MyWS2812         # Modul WS2812  -> WS2812-Ansteuerung
-        #print("WS2812 -> Setup")
-        MyWS2812.setup_ws2812()
-        ### Test ###
-        #print("WS2812 -> Dot-Test")
-        MyWS2812.test_rotate()
-
-    if MyModule.inc_decoder:
-        #print("Decode -> Load-Module")
-        import module_decode as MyDecode
-        #print("Decode -> Setup")
-        MyDecode.decode_setup()
-        ### Test ###
-        #print("Decode -> Test")
-        #MyDecode.decode_input("Test")
-
-    if MyModule.inc_serial:
-        #print("Serial-COM -> Load-Module")
-        import module_serial as MySerial
-        #print("Serial-Con -> Setup")
-        MySerial.sercon_setup()
-        ### Test ###
-        #print("Serial-Con -> Test")
-        #MySerial.sercon_write_out("Start Test")
 
     main()      # Start Main $$$
 
